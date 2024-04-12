58aaf7ed7158b13fdc22339d0e6989e5eaa25652
Erneto Alfonso <ealfonsov89@users.noreply.github.com>
2024-04-12 11:16:20 +0000

    Refactor TestApplication.java to remove unused endpoint

---
+++ src/main/java/com/tales/test/Models/Greeting.java
@@ -0,0 +1,4 @@
+package com.tales.test.Models;
+
+public record Greeting(long id, String content) {}
+
--- src/main/java/com/tales/test/TestApplication.java
+++ src/main/java/com/tales/test/TestApplication.java
@@ -2,8 +2,6 @@ package com.tales.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class TestApplication {
@@ -11,9 +9,4 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
-	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
-
 }
