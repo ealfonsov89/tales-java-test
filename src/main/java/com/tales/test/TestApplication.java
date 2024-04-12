8b7d90f0521d18e37866565811ce7c2938b709ee
Erneto Alfonso <ealfonsov89@users.noreply.github.com>
2024-04-12 10:59:23 +0000

    Update sourceCompatibility to Java 21 in build.gradle Add hello endpoint to TestApplication.java

--- src/main/java/com/tales/test/TestApplication.java
+++ src/main/java/com/tales/test/TestApplication.java
@@ -2,6 +2,8 @@ package com.tales.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class TestApplication {
@@ -9,5 +11,9 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
