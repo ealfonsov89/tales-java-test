d80025c61556b45de76f3984ef9f97742b6c4962
Erneto Alfonso <ealfonsov89@users.noreply.github.com>
2024-04-12 18:15:09 +0000

    Add Swagger configuration and dependencies

--- src/main/java/com/tales/test/controllers/GreetingController.java
+++ src/main/java/com/tales/test/controllers/GreetingController.java
@@ -3,12 +3,14 @@ package com.tales.test.controllers;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tales.test.Models.Greeting;
 
 @RestController
+@RequestMapping("greeting")
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
